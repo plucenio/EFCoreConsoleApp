9f4a9733e2c820f567952968ab9027d9abbbfbfe
filipe.plucenio <plucenio@hotmail.com>
2018-12-12 15:52:59 -0200

    Adicionado demais arquivos

---
+++ EFCoreConsoleApp/Context/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreConsoleApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreConsoleApp.Context
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Cidade> Cidades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cidade>().HasData(new Cidade { Id = 1, Nome = "Torres" });
+            modelBuilder.Entity<Cidade>().HasData(new Cidade { Id = 2, Nome = "Porto Alegre" });
+            modelBuilder.Entity<Cidade>().HasData(new Cidade { Id = 3, Nome = "Canoas" });
+            modelBuilder.Entity<Cidade>().HasData(new Cidade { Id = 4, Nome = "Viamão" });
+            modelBuilder.Entity<Cidade>().HasData(new Cidade { Id = 4, Nome = "Pelotas" });
+        }
+    }
+}
---
+++ EFCoreConsoleApp/Entities/Cidade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreConsoleApp.Entities
+{
+    public class Cidade : Entity
+    {
+        public string Nome { get; set; }
+    }
+}
